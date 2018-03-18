5ee2d505ca6d8a5f601c7618a8830c01bf2ee901
alarivas <alarivas@udec.cl>
2018-03-18 00:34:43 -0300

    config heroku

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
